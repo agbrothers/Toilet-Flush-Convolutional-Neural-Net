ed2f0b8eee252b5f89a7bb5026b1e3f8823211be
Greyson Brothers <greysonbrothers@gmail.com>
2020-04-09 15:47:51 -0700

    Update and rename 2.py to CNN.py

--- CNN.py
+++ CNN.py
@@ -1,6 +1,5 @@
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
-# matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -25,22 +24,6 @@ from keras.utils import np_utils
 from keras import backend as K
 from PIL import Image
 
-
-
-""" ARGPARSE """
-"""
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset (i.e., directory of images)")
-ap.add_argument("-m", "--model", required=True,
-	help="path to output model")
-ap.add_argument("-l", "--labelbin", required=True,
-	help="path to output label binarizer")
-ap.add_argument("-p", "--plot", type=str, default="plot.png",
-	help="path to output accuracy/loss plot")
-args = vars(ap.parse_args())
-"""
 
 # initialize the number of epochs to train for, initial learning rate,
 # batch size, and image dimensions
@@ -76,7 +59,6 @@ print('LOADING COMPLETE')
 """ ASSIGN DATA TO TESTING SETS """
 data = np.array(data)/255
 labels = np.array(labels)
-
 # 60 - 20 - 20 split of the data
 X_train, X_test, Y_train, Y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=42)
@@ -87,13 +69,6 @@ y_train = np_utils.to_categorical(encoder.fit_transform(Y_train))
 y_val = np_utils.to_categorical(encoder.fit_transform(Y_val))
 y_test = np_utils.to_categorical(encoder.fit_transform(Y_test))
 num_classes = len(y_test[0])
-
-""" OPTIONAL AUGMENTATION """
-# construct the image generator for data augmentation
-#aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
-#	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-#	horizontal_flip=True, fill_mode="nearest")
-
 
 
 """ CONSTRUCT THE MODEL """
@@ -172,14 +147,11 @@ model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
 
-
 """ TRAIN THE MODEL """
 print("TRAINING MODEL...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 history = model.fit(X_train, y_train, batch_size=BS, epochs=EPOCHS, validation_data=(X_val, y_val))
-
-
 
 """ PRINT ACCURACY """     
 result = model.predict(X_test)
@@ -187,13 +159,11 @@ correct = [(np.argmax(y_test[i])== np.argmax(result[i])) for i in range(len(y_te
 acc = round(100*sum(correct)/len(correct), 2)
 print("Accuracy: ", round(acc, 2), "%", sep='')
 
-
 """ PLOT CONFUSION MATRIX """
 y_pred = [np.argmax(i) for i in result]
 y_pred = pd.DataFrame(encoder.inverse_transform(y_pred))
 conf_mat = confusion_matrix(Y_test, y_pred, labels=encoder.classes_)
 conf_mat = [np.around(i/sum(i),2) for i in conf_mat]  # normalize entries
-
 fig, ax = plt.subplots(figsize=(9,8))
 ax = sns.heatmap(conf_mat, cmap='Blues', annot=True, xticklabels=encoder.classes_, yticklabels=encoder.classes_)
 ax.tick_params(labelsize=6)
@@ -201,8 +171,6 @@ plt.title('2 Accuracy: ' + str(acc))
 plt.xlabel('Predictions')
 plt.ylabel('True Values')
 plt.tight_layout()
-
-
 
 # Plot training & validation accuracy values
 fig1 = plt.figure(figsize=(6,8))
@@ -223,39 +191,3 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.tight_layout()
-
-
-
-
-# 50 epochs, 79.13%
-
-# 7 layers 
-#   Accuracy: 72.17%
-
-# 7 Layers, 0.1 dropout
-#   Accuracy: 75.65%
-
-# 7 Layers, 0.15 dropout 
-#   Accuracy: 72.17%
-#   Accuracy: 70.43%
-
-# 7 Layers, 0.1 dropout, 100 epochs, 
-#   Accuracy: 77.39%
-#   Accuracy: 81.74%
-#   Accuracy: 80.87%
-#   Accuracy: 76.52%
-#   Accuracy: 73.91%
-#   Accuracy: 74.78%
-#   Accuracy: 81.74%
-
-
-
-
-
-
-
-
-
-
-
-
